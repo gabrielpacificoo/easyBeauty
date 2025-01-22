317c5189dfbda765443f631314f419436a022fb1
gabrielpacificoo <gabriel.pacifico@sptech.school>
2025-01-21 21:11:10 -0300

    Setup commit

---
+++ src/routes/compra.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+var controllerCompra = require('./controllers/compra.js')
+
+router.post('/registro', (req, res) => {
+  
+})
+
+module.exports = router;
---
+++ src/routes/usuario.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/cadastro', (req, res) => {
+
+})
+
+router.get('/autenticar/:emailUsuario/:senhaUsuario', (req, res) => {
+
+})
+
+module.exports = router;
